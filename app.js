ceca32c5de9592d7efc46f3b757bafce40cccd91
Nightz <peerapat.ud@gmail.com>
2022-04-16 13:08:40 +0700

    Create function computerPlay and playRound

---
+++ app.js
@@ -0,0 +1,40 @@
+function computerPlay() {
+    // Generate random result of rock, paper, scissors for computer
+    let random_result = Math.random();
+    if (random_result >= 2/3) {
+        return 'rock';
+    }
+    else if (1/3 <= random_result < 2/3) {
+        return 'paper';
+    }
+    else    
+        return 'scissors';
+}
+
+function playRound(playerSelection,computerSelection) {
+    // Convert playerSelection to lowercase for case-insensitive
+    playerSelection = playerSelection.toLowerCase();
+
+    // Find winner of round
+    if ((playerSelection === "rock" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection == "paper") || (playerSelection === "scissors" && computerSelection === "scissors")) {
+        return "Tie";
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Won! Rock beats Scissors";
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper")   {
+        return "You Lose! Paper beats Rock";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock")   {
+        return "You Won! Paper beats Rock";
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors")   {
+        return "You Lose! Scissors beats Paper";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock")   {
+        return "You Lose! Rock beats Scissors";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper")   {
+        return "You Won! Scissors beats Paper";
+    }
+}
