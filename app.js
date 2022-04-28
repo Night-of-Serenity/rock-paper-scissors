95268336bd2f5e96905bcb7bd6911a050c5b05b6
Nightz <peerapat.ud@gmail.com>
2022-04-28 13:25:44 +0700

    Rework app.js to work with UI

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const btnRock = document.querySelector('#btnRock');
+const btnPaper = document.querySelector('#btnPaper');
+const btnScissors = document.querySelector('#btnScissors');
+const scoreMessage = document.querySelector('.score-message');
+const playerNumber = document.querySelector('#player-number');
+const comNumber = document.querySelector('#com-number');
+
+btnRock.addEventListener('click', playerClick);
+btnPaper.addEventListener('click', playerClick);
+btnScissors.addEventListener('click', playerClick);
+
+let playerWin = 0;
+let comWin = 0;
+let round = 5;
+
+
 function computerPlay() {
     // Generate random result of rock, paper, scissors for computer
     let random_result = Math.floor(Math.random() * 3);
@@ -16,74 +32,72 @@ function computerPlay() {
 }
 
 function playRound(playerSelection,computerSelection) {
-    // Convert playerSelection to lowercase for case-insensitive
-    playerSelection = playerSelection.toLowerCase();
-
+    console.log(playerSelection + ' ' +computerSelection);
     // Find winner of round
     if (playerSelection === computerSelection) {
         return "You Tie!";
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerWin++;
         return "You Won! Rock beats Scissors";
     }
     else if (playerSelection === "rock" && computerSelection === "paper")   {
+        comWin++;
         return "You Lose! Paper beats Rock";
     }
     else if (playerSelection === "paper" && computerSelection === "rock")   {
+        playerWin++;
         return "You Won! Paper beats Rock";
     }
     else if (playerSelection === "paper" && computerSelection === "scissors")   {
+        comWin++;
         return "You Lose! Scissors beats Paper";
     }
     else if (playerSelection === "scissors" && computerSelection === "rock")   {
+        comWin++;
         return "You Lose! Rock beats Scissors";
     }
     else if (playerSelection === "scissors" && computerSelection === "paper")   {
+        playerWin++;
         return "You Won! Scissors beats Paper";
     }
 }
 
-function game(round) { 
-    // Simulate number of rounds game 
-    let win = 0;
-    let lose = 0;
-    let playerSelection;
-    let computerSelection;
-    let play_round;
-    // Iterate loops for number of rounds
-    for (let i = 0; i < round; i++) {
-        // Player choose input
-        playerSelection = prompt("Choose 'rock' 'paper' 'scissors'");
 
-        // Generate random result for computer
-        computerSelection = computerPlay();  
-
-        // Simulate one round of game
-        play_round= playRound(playerSelection,computerSelection);
-        console.log(play_round);
-
-        // Record score
-        if (play_round.includes("Won")) {
-            win++;
-        }
-        else if(play_round.includes("Lose")) {
-            lose++;
-        }
+function playerClick(e) {
+    // Get attribute id-player-pick value from clicked image
+    const playerSelection = e.target.attributes[0].value;
+    if (round > 0) {
+        round--;
+        scoreMessage.textContent = playRound(playerSelection,computerPlay());
+        updateScore();
+        checkWinner();
     }
-
-    // Find winner which won more rounds
-    if  (win > lose) {
-        console.log("You are winner!");
-    }
-    else if (win < lose) {
-        console.log("Computer is winner!");
-    }
-    else 
-        console.log("No winner!");
+    else return;
 }
 
-// Prompt player for input number of rounds
-const round = 5;
+function checkWinner() {
+    if (round === 0) {
+        if (playerWin > comWin) {
+            scoreMessage.textContent = 'YOU WIN!';
+            scoreMessage.classList.add('winner');
+            return;
+        }
+        else if (comWin > playerWin) {
+            scoreMessage.textContent = 'YOU LOSE!';
+            scoreMessage.classList.add('winner');
+            return;
+        } 
+        else {
+            scoreMessage.textContent= 'GAME TIE!';
+            scoreMessage.classList.add('winner');
+            return;
+        }
+    }
+    else return;
+}
 
-game(round);
-
+function updateScore() {
+    playerNumber.textContent = `${playerWin}`;
+    comNumber.textContent = `${comWin}`;
+}
